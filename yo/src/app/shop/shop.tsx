45a732801db5eb68ec7beb86472fe27f47a078be
HaQadosch <haqadosch@pm.me>
2023-01-08 08:28:00 +0000

    link the comps together

--- yo/src/app/shop/shop.tsx
+++ yo/src/app/shop/shop.tsx
@@ -1,12 +1,15 @@
+import { Banner } from '@yo/shared/ui';
+import { Link } from 'react-router-dom';
 import styles from './shop.module.css';
 
 /* eslint-disable-next-line */
-export interface ShopProps {}
+export interface ShopProps { }
 
-export function Shop(props: ShopProps) {
+export function Shop (props: ShopProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Shop!</h1>
+    <div className={ styles['container'] }>
+      <Banner text="Here is a list of products to buy..." />
+      <Link to="/cart">View Cart</Link>
     </div>
   );
 }
